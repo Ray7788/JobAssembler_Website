6db83d09cbb6f1f6221c8fe9f038b8e94c98c5ef
DevM-uk <19396918+DevM-uk@users.noreply.github.com>
2022-02-18 14:14:39 +0000

    Created job class

---
+++ classes/job.php
@@ -0,0 +1,12 @@
+<?php
+
+class Job
+{
+    public int $id;
+    public string $title;
+    public string $description;
+
+    public static function create_job(string $title, string $description, string $location, int $company_id) {
+
+    }
+}
