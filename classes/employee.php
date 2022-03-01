ccf98caf7a95e95440dad3cefa852b8470cdb36a
Matthew Chee <matthew.chee@student.manchester.ac.uk>
2022-03-01 23:31:56 +0000

    Employee class

---
+++ classes/employee.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("prevent_direct_access.php");
+require_once("database.php");
+
+class employee
+{
+	public int $id;
+	public String $email;
+	public String $biography;
+
+	public static function create_employee(String $biography){
+		$pdo = Database::connect();
+		$query = "INSERT INTO 'UserAccounts'('Biography') VALUES(':biography')";
+		$statement = $pdo->prepare($query);
+		return $statement->execute([
+			"biography" => $biography
+		]);
+	}
+}
