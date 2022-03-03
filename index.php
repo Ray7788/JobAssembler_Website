91c426d924f4727028a222d348c1c26b39c3b6e1
p00331rx <ray778@foxmail.com>
2022-03-03 16:53:04 +0000

    background finshed

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/index-Normalise-custom.css"> <!-- Include a custom CSS for the website later-->
+    <link rel="stylesheet" href="CSS/index-Normalise-custom.css"> <!-- Include a custom CSS for the website. -->
 
     <title>JobAssembler</title>
 
     <style>
         * {
-        box-sizing: border-box;
         margin: 0;
         padding: 0;
         border: 0;
@@ -26,31 +25,33 @@
             height: 100vh;
         }
 
+        /* Delete extra scroll bars */
+        body {
+            overflow: hidden;
+        }
+
+        /* For every pages */
+        .container-fluid {
+            padding:0%; margin: 0%;
+        }
+
+/* ----------------------------------------------------------------------------------------------------------------- */
         /* For Logo border*/
         .d-inline-block-align-top {border-radius: 5px;}
 
         /* For navbar */
         .container-fluid {
             scroll-snap-align: start;
-            width: 100vw;
+            width: 100vh;
             height: 100vh;
         }
-
-        /* delete extra scroll bars */
-        body {
-            overflow: hidden;
-        }
-
-        /* For every page */
-        .container-fluid {
-            padding:0%; margin: 0%;
-        }
-
+/* ----------------------------------------------------------------------------------------------------------------- */
         /* 1st page */
         .page1 {
       
-            width: 120vw;
-            height: 170vh;
+            width: 100vw;
+            height: 100vh;
+            border-width:0px ;
             background: linear-gradient(
             135deg,
             hsl(170deg, 80%, 70%),
@@ -63,7 +64,7 @@
          /*3rd page */
         .page3 {
             width: 100vw;
-            height: 150vh;
+            height: 100vh;
             background: linear-gradient(
                 135deg,
             rgb(225, 238, 210),
@@ -76,7 +77,7 @@
         /* 2nd page */
         .page2 {
             width: 100vw;
-            height: 150vh;
+            height: 100vh;
             background: linear-gradient(
                 135deg,
             rgb(226, 241, 141),
@@ -94,6 +95,13 @@
         100% {
           background-position: 100% 100%;
         }
+    }
+/* ----------------------------------------------------------------------------------------------------------------- */
+
+        .display-4 {text-align:center;}
+        .display-6 {text-align:center;}
+    
+
     </style>
   </head>
 
@@ -111,49 +119,45 @@
         </a>
     </nav>
    
+
     <main>
     <!-- page1 -->
     <div class="container-fluid">
-        <div class="jumbotron text-center mt-5 mx-2" style="height: 600px;">
-            <div class="page1">
+        <div class="page1">
             <h1 class="display-4" style="color: #5ebec4;">Struggling with finding Employment or Employees?</h1>
             <h1 class="display-6" style="color: #5ebec4;">Ever wish it was as easy as Tinder?</h1>
             <hr class="my-3">
             <p class="lead" style="color: #5ebec4;">JobAssembler Is!</p>
-            <a class="btn btn-primary btn-lg" href="SignUpPage2.php" role="button" style="background-color: #f92c85;">Sign Up Now!</a>
-        </div>
+            <a class="btn-btn-primary-btn-lg" href="SignUpPage2.php" role="button" style="background-color: #f92c85;">Sign Up Now!</a>
+
         </div> 
     </div>
 
     <!-- page2 -->
     <div class="container-fluid">
-        <div class="jumbotron text-center mt-5 mx-2" style="height: 600px;">
             <div class="page2">
             <h1 class="display-4" style="vertical-align:-webkit-baseline-middle; color: #FFFFCC"> Direct Contact Between Employers and Employees  :)</h1>
             <h1 class="display-6" style="color: #FFFFCC;">Customized for campus students!</h1>
             <hr class="my-4">
-            <p class="lead" style="color: #5ebec4;"></p>
-            <a class="btn btn-primary btn-lg" href="SignUpPage2.php" role="button">Sign Up Now!</a>
-            </div>
+            <a class="btn-btn-primary-btn-lg" href="SignUpPage2.php" role="button">Sign Up Now!</a>
         </div>
     </div>
 
     <!-- page3 -->
     <div class="container-fluid">
-        <div class="jumbotron text-center mt-5 mx-2" style="height: 600px;">
             <div class="page3">
             <h1 class="display-4" style="color: #7da2a9;">The Best of the Best</h1>
-            <a class="btn btn-primary btn-lg" href="SignUpPage2.php" role="button">Sign Up Now!</a>
-            </div>
+            <a class="bt-btn-primary-btn-lg" href="SignUpPage2.php" role="button">Sign Up Now!</a>  
         </div>
     </div>
 
     </main>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
-</main>
+
     </body>
 </html>
