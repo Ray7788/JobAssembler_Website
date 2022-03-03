6778e6c8328f8d69e9e3df41f316eafd77c630f0
DevM-uk <19396918+DevM-uk@users.noreply.github.com>
2022-03-03 01:51:01 +0000

    Added option buttons to JobList.php page

--- JobList.php
+++ JobList.php
@@ -27,6 +27,9 @@ $jobs = $statement->fetchAll();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Viewed Jobs</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
 <main class="container" style="padding-top: 2rem">
@@ -41,16 +44,29 @@ $jobs = $statement->fetchAll();
                 <th scope="col">About</th>
                 <th scope="col">Location</th>
                 <th scope="col">Accepted</th>
+                <th scope="col"></th>
             </tr>
         </thead>
         <tbody>
-            <?php foreach($jobs as $line): ?>
+            <?php foreach($jobs as $num => $line): ?>
             <tr>
                 <td><?= $line["JobID"]?></td>
                 <td><?= $line["Title"]?></td>
                 <td style="word-break:break-all;"><?= $line["Details"]?></td>
                 <td><?= "Unknown"?></td>
                 <td><input type="checkbox" disabled <?= $line["UserAccepted"] ? "checked": ""?>></td>
+                <td>
+                    <div class="dropdown show">
+                        <button class="btn btn-sm dropdown-toggle" href="" role="button" id="dropdownMenuLink<?=$num?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Options
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink<?=$num?>">
+                            <button class="dropdown-item btn" href="">View details</button>
+                            <button class="dropdown-item btn" href="">Change acceptance</button>
+                            <button class="dropdown-item btn btn-danger" href="">Report</button>
+                        </div>
+                    </div>
+                </td>
             </tr>
             <?php endforeach; ?>
         </tbody>
