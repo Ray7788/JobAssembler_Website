b99c143d745f70fd7c2c946b28250ff193777bac
DevM-uk <19396918+DevM-uk@users.noreply.github.com>
2022-03-07 11:45:48 +0000

    Finally found a fix to make login API work for all PHP 7 versions

--- classes/api_response_generator.php
+++ classes/api_response_generator.php
@@ -4,14 +4,16 @@ use JetBrains\PhpStorm\NoReturn;
 
 class ApiResponseGenerator
 {
-    #[NoReturn] public static function generate_error_json(int $response_code, string $message) {
+    #[NoReturn]
+    public static function generate_error_json(int $response_code, string $message) {
         header("Content-Type: application/json; charset=UTF-8");
         http_response_code($response_code);
         echo(json_encode(array("message" => $message)));
         die(1);
     }
 
-    #[NoReturn] public static function generate_response_json(int $response_code, array $data) {
+    #[NoReturn]
+    public static function generate_response_json(int $response_code, array $data) {
         header("Content-Type: application/json; charset=UTF-8");
         http_response_code($response_code);
         echo(json_encode($data));
