507b4868e3a39c707895ba93cb3414ed56356834
Kv1n20 <kavin.gunasekara@student.manchester.ac.uk>
2022-03-08 09:41:43 +0000

    login troubleshooting

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
-    <title>Signin - Job Assembler/title></title>
+    <title>Signin - Job Assembler</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sign-in/">
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -62,7 +62,7 @@
   <body class="text-center">
     
 <main class="form-signin">
-    <img class="mb-4" src="/Images/Logo1.png" alt="" width="72" height="57">
+    <img class="mb-4" src="Images/Logo1.png" alt="" width="72" height="57">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <form name="loginForm" id="loginForm">
