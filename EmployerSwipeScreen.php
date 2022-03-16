012873589aa512f0e9fdc45ed4975e4e5878a4c5
james chesterman <chesterman261@sky.com>
2022-03-16 13:51:01 +0000

    List of jobs added. Can click on one.

--- EmployerSwipeScreen.php
+++ EmployerSwipeScreen.php
@@ -18,28 +18,21 @@ $pdo = Database::connect();
 $query = "SELECT * FROM JobPostings WHERE CompanyID = :companyID";
 $statement = $pdo->prepare($query);
 $statement->execute(["companyID" => $user->company_id]);
-$data = $statement->fetchAll();
-
-
-
-//Now do the thing that gets the jobs you haven't seen yet.
-$columns = array("JobID", "Title", "Details", "CompanyID", "UserSeen", "CompanyID", "Name", "Description", "CompanyImage");
-//It's SELECT DISTINCT because without it the same row is returned multiple times. I think it's because my INNER JOIN stuff is outdated but this solves it easily.
-$query = "SELECT DISTINCT JobPostings.*, UserJobs.UserSeen, Companies.* FROM ((JobPostings
-INNER JOIN UserJobs ON JobPostings.JobID = UserJobs.JobID)
-INNER JOIN Companies ON JobPostings.CompanyID = Companies.CompanyID) 
-WHERE UserJobs.UserSeen = 0 AND UserJobs.UserID=:userID;";
-$statement = $pdo->prepare($query);
-$statement->execute(["userID" => $userID]);
-$data = $statement->fetchAll();
-$jobs = array_reverse($data);
-
+$jobs = $statement->fetchAll();
+$num=1;
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <title>Employer Swipe Screen</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        
         <style type="text/css">
             .container{
                 justify-content:center;
@@ -77,18 +70,29 @@ $jobs = array_reverse($data);
                 background-color:aqua;
             }
         </style>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        
         <script>
             var userID = <?php echo($userID); ?>;
-            
-            
+            var currentJob = 0;
+            $(function(){
+                $(".dropdown-menu a").click(function(){
+                    alert($(this).text());
+                    currentJob = $(this).text();
+                });
+            });
+
+            function writeToCard(){
+                
+            }
         </script>
     </head>
     <body>
         <?php
             echo("You are signed in as: " . $user->username);
+            echo(implode("",$jobs[0]));
+            
         ?>
-
+        
         <div class="container">
             <div class="box">
                 <p id="card" name="card">
@@ -100,11 +104,26 @@ $jobs = array_reverse($data);
         </div>
         <div class="buttonHolder">
 
-            <button class="button" id="noButton" onclick="buttonPressed(0)">NO</button>
-            <button class="button" id="yesButton" onclick="buttonPressed(1)">YES</button>
+            <!--<button class="button" id="noButton" onclick="buttonPressed(0)">NO</button>
+            <button class="button" id="yesButton" onclick="buttonPressed(1)">YES</button>-->
+
+        </div>
+        <div class="buttonHolder">
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                    Dropdown button
+                </button>
+                <div class="dropdown-menu">
+                    <?php
+                        for($x=0; $x<count($jobs);$x++){
+                            echo('<a class="dropdown-item">' . $jobs[$x][1]);
+                        }
+                    ?>
+                </div>
+            </div>
         </div>
     </body>
     <script>
-        writeToCard();
+        //writeToCard();
     </script>
 </html>
