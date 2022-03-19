1fee606dde1e2c288c57081499b05117ab3155ef
james chesterman <chesterman261@sky.com>
2022-03-19 14:03:24 +0000

    Now switches what users you can see depending on what job you select.

--- EmployerSwipeScreen.php
+++ EmployerSwipeScreen.php
@@ -24,17 +24,25 @@ $statement = $pdo->prepare($query);
 $statement->execute(["companyID" => $companyID]);
 $jobs = $statement->fetchAll();
 
-//Make the array of jobs belonging to the company into a string ready for the sql query.
+$jobString = "";
+//Make the array of job IDs belonging to the company into a string ready for the sql query.
+//Want it like 2,3,4
+for($x=0; $x<count($jobs); $x++){
+    $jobString .= $jobs[$x][0];
+    $jobString .= ",";
+}
+$jobString = substr($jobString, 0, -1); //Removes the last comma
 
 
 //* FROM UserAccounts WHERE UserJobs.jobID belongs to my companyID AND CompanySeen = 0.
-$query = "SELECT DISTINCT UserAccounts.UserID, UserAccounts.Forename, UserAccounts.Surname, UserAccounts.Biography FROM ((UserAccounts 
+$query = "SELECT DISTINCT UserAccounts.UserID, UserAccounts.Forename, UserAccounts.Surname, UserAccounts.Biography, UserJobs.JobID FROM ((UserAccounts 
 INNER JOIN UserJobs ON UserJobs.UserID = UserAccounts.UserID) 
 INNER JOIN JobPostings ON JobPostings.JobID = UserJobs.JobID) 
-WHERE UserJobs.UserAccepted = 1 AND UserJobs.CompanySeen = 0 AND UserJobs.JobID in (2, 3, 4);";
+WHERE UserJobs.UserAccepted = 1 AND UserJobs.CompanySeen = 0 AND UserJobs.JobID in (" . $jobString . ");";
+//Printing two of each person because
 $statement = $pdo->prepare($query);
 $statement->execute(["companyID" => $companyID]);
-$userAccounts = $statements->fetchAll();
+$userAccounts = $statement->fetchAll(PDO::FETCH_NUM);
 ?>
 
 <!DOCTYPE html>
@@ -76,7 +84,7 @@ $userAccounts = $statements->fetchAll();
                 margin-right:auto;
                 border: none;
                 color: black;
-                padding: 15px 32px;
+                padding: 30px 60px;
                 text-align: center;
                 text-decoration: none;
                 display: inline-block;
@@ -88,26 +96,60 @@ $userAccounts = $statements->fetchAll();
         
         <script>
             var userID = <?php echo($userID); ?>;
-            var currentJobID = 0;
             var jobArray = <?php echo json_encode($jobs) ?>;    //If this is empty, disable buttons
+            var currentJobID = jobArray[0][0];
             var currentJob = jobArray[currentJobID][1];
-            //var userAccounts = <?php echo json_encode($userAccounts) ?>;  //If this is empty, there are no users left to swipe through.
-            
+            var userAccounts = <?php echo json_encode($userAccounts) ?>;  //If this is empty, there are no users left to swipe through.
+            var userCounter = 0;
+            var userAccountsForJob = [];
+
             $(function(){
                 $(".dropdown-menu a").click(function(){
-                    alert($(this).text());
                     currentJob = $(this).text();
+                    currentJobID = $(this).attr('id');      //Gets the ID of the dropdown item
+                    currentJobID = currentJobID.substring(8); //Every ID has 'dropdown' at the start. Remove it to just get the number
+                    //Right now 'currentJobID' is only relative to the list in the dropdown. Need to make it relative to the array from the database
+                    currentJobID = jobArray[currentJobID][0];
+                    getRightJobs();
                     document.getElementById("jobName").innerHTML = "Current job: " + currentJob;
                 });
             });
 
             function getRightJobs(){
+                //NOT GETTING THE RIGHT JOBS AT THE MOMENT. maybe currentjobid is always 2.
+                userAccountsForJob = [];
+                userCounter = 0;
                 //When user presses the dropdown button and changes jobs, you want to change the jobs displayed
                 //Get array of jobs with jobID just selected.
+                for(let i=0; i<userAccounts.length;i++){
+                    console.log(userAccounts[i].join());
+                    if(userAccounts[i][4] == currentJobID){
+                        console.log(currentJobID);
+                        //console.log(userAccounts[i].join());
+                        userAccountsForJob.push(userAccounts[i]);
+                    }
+                }
+                if(userAccountsForJob.length == 0){
+                    document.getElementById("card").innerHTML = "Sorry, you've seen all available job applicants.";
+                    document.getElementById("noButton").disabled = true;
+                    document.getElementById("yesButton").disabled = true;
+                }else{
+                    writeToCard();
+                }
             }
 
             function writeToCard(){
-                //var forename = 
+                var forename = userAccountsForJob[userCounter][1];
+                var surname = userAccountsForJob[userCounter][2];
+                var biography = userAccountsForJob[userCounter][3];
+                
+                document.getElementById("card").innerHTML = ("Forename: " + forename + " <br> "
+                + "Surname: " + surname + " <br> "
+                + "Biography: " + "<br> <textarea columns=120 rows=4 readonly>" + biography + "</textarea><br>");
+            }
+
+            function buttonPressed(yesOrNo){
+                //dataArray = {"companyAccepted":yesOrNo, "userID":userAccounts[currentUser][0], "jobID":currentJobID};
             }
         </script>
     </head>
@@ -115,7 +157,7 @@ $userAccounts = $statements->fetchAll();
         <?php
             echo("You are signed in as: " . $user->username);
             //echo(implode(",",$jobs[0]));
-            echo($user->companyID);
+            //echo(implode(",", $userAccounts[0]));
         ?>
         <p id="jobName" name="jobName">
             placeholder
@@ -124,16 +166,14 @@ $userAccounts = $statements->fetchAll();
         <div class="container">
             <div class="box">
                 <p id="card" name="card">
-                    
+                    Sorry, you've seen all available jobs.
                 </p>
-                
-
             </div>  
         </div>
         <div class="buttonHolder">
 
-            <!--<button class="button" id="noButton" onclick="buttonPressed(0)">NO</button>
-            <button class="button" id="yesButton" onclick="buttonPressed(1)">YES</button>-->
+            <button class="button" id="noButton" onclick="buttonPressed(0)">NO</button>
+            <button class="button" id="yesButton" onclick="buttonPressed(1)">YES</button>
 
         </div>
         <div class="buttonHolder">
@@ -144,7 +184,8 @@ $userAccounts = $statements->fetchAll();
                 <div class="dropdown-menu">
                     <?php
                         for($x=0; $x<count($jobs);$x++){
-                            echo('<a class="dropdown-item">' . $jobs[$x][1]);
+                            echo('<a class="dropdown-item" id="dropdown'.$x.'">' . $jobs[$x][1]);
+                            //set ID to dropdown$x  8
                         }
                     ?>
                 </div>
@@ -153,6 +194,7 @@ $userAccounts = $statements->fetchAll();
     </body>
     <script>
         document.getElementById("jobName").innerHTML = "Current job: " + currentJob;
-        //writeToCard();
+        getRightJobs();
+        writeToCard();
     </script>
 </html>
