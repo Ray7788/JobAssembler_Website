4ca397095c82a75f844a7727a4f3e0e1b9635c04
Ray7788 <ray778@foxmail.com>
2022-03-19 15:14:20 +0000

    JobCreation Page Styling (unformal)

--- JobCreation.php
+++ JobCreation.php
@@ -2,6 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Job Creation - JobAssembler</title>
+
+   <!-- CSS Styling-->
+    <link rel="stylesheet" href="CSS/JobCreation.css">
+    <link rel="stylesheet" href=../JobAssembler/CSS/JobCreation.css>
+    <!-- Bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script>
         // Shamelessly stole this directly from James's code
@@ -27,38 +37,78 @@
                 })
             });})
     </script>
-    <title>Job Creation  JobAssembler</title>
 
-    <!-- CSS Styling-->
-    <link rel="stylesheet" href="CSS/JobCreation.css">
-    <link rel="stylesheet" href=../JobAssembler/CSS/JobCreation.css>
+<style>
+    body {
+        display: flex;
+        justify-content: center;
+        background-image: linear-gradient(to left, #e1eec3, #f05053);
+    }
+    .container-fluid{
+        background-color: #fff;
+        width: 550px;
+        height: 750px;
+        position: relative;
+        display: flex;
+        border-radius: 10px;
+        justify-content: center;
+        align-items: center;
+        top: 50px;
+    }
+    .b{
+        width: 400px;
+        height: 650px;
+        overflow: hidden;
+    }
+    .header1{
+        font: 900 24px '';
+        margin: 5px 0;
+        text-align: center;
+        letter-spacing: 3px;
+    }
+    .e{
+        width: 100%;
+        margin-bottom: 20px;
+        outline: none;
+        border: 0;
+        padding: 10px;
+        border-bottom: 2px solid rgb(60,60,70);
+        font: 900 16px '';
+    }
+
+</style>
 </head>
 <body>
-    <div class="content">
-        <div class="login-box">
-            <h1>Enter Job Detail</h1>
-            
+    <div class="container-fluid">
+    <div class="b">
+
+
     <form name="jobCreateForm" id="jobCreateForm">
-        <!-- <p> -->
-            <div class="output-frame">
-            <label for="username">Job title</label>
-            <input type="text" name="title" id="title" required><br><br>
-            </div>
+        <p>
+        <h1 class="header1">Enter Job Detail</h1>
+        <br><br>
 
-            <div class="output-frame">
-            Job description<br>
-            <textarea type="textbox" name="description" id="description" rows="4" cols="50" required></textarea><br><br>
-            </div>
-
-            <div class="output-frame">
-            Job Location<br>
-            <input type="text" name="location" id="location" required><br><br>
-            </div>
+            <h3 class="d">Job Title<br></h3>
+            <input type="text" name="title" class="e" id="title"  placeholder="Input Title" required>
+            <br><br>
+            <h3 class="d">Job Description</h3>
+            <textarea type="textbox" name="description" class="e" id="description" rows="3" cols="50" placeholder="More Details" required></textarea>
+            <br><br>
+            <h3 class="d">Job Location</h3> 
+            <input type="text" name="location" class="e" id="location"  placeholder="Post Code" required>
+            <br><br>
+         
+            <!-- button -->
+            <input type="submit" class="f" value="Submit">
             
-            <input type="submit" value="Submit">
-        <!-- </p> -->
+        </p>
+       
+        
     </form>
-        </div>
+    
     </div>
+    </div>
+
+    <!-- <p class="mt-5 mb-3 text-muted">&copy; X17 2021-2022</p> -->
 </body>
 </html>
