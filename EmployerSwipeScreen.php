643d6e15b2acf4cc884b9b1d38caea053411ec87
DevM-uk <19396918+DevM-uk@users.noreply.github.com>
2022-03-29 14:32:01 +0100

    Updated JQuery library for EmployerSwipeScreen.php

--- EmployerSwipeScreen.php
+++ EmployerSwipeScreen.php
@@ -55,11 +55,10 @@ $userAccounts = $statement->fetchAll(PDO::FETCH_NUM);
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
         
         <!-- Jquery -->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-        
+
         <style type="text/css">
             body {
             width: 100vw;
