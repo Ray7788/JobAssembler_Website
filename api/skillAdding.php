f655da23570fce69d42180c80452046d355e11a4
james chesterman <chesterman261@sky.com>
2022-03-30 01:30:15 +0100

    JobSkills page (almost) done. Need to get JobID of the job currently selected from EmployerSwipeScreen. (Need to add links to userSkills and JobSkills from each swipe screen.

--- api/skillAdding.php
+++ api/skillAdding.php
@@ -8,45 +8,73 @@ if($_SERVER["REQUEST_METHOD"] !== "POST"){
     ApiResponseGenerator::generate_error_json(405, "{$_SERVER["REQUEST_METHOD"]} method not alllowed");
 }
 $languages = ["java", "python", "csharp", "html", "php", "css", "cplus", "sql"];
-$required_params = ["userID", "javaYears", "pythonYears", "csharpYears", "htmlYears", "phpYears", "cssYears", "cplusYears", "sqlYears", "emoIntel", "patience", "adapt", "projManage","probSolv","teamwork","interpersonal","leadership","time","decisiveness"];
+$required_params = ["userID", "javaYears", "pythonYears", "csharpYears", "htmlYears", "phpYears", "cssYears", "cplusYears", "sqlYears", "emoIntel", "patience", "adapt", "projManage","probSolv","teamwork","interpersonal","leadership","time","decisiveness", "isCompany", "jobID"];
 foreach($required_params as $param){
     if(!isset($_REQUEST[$param])){
         ApiResponseGenerator::generate_error_json(400, "Parameter $param not set.");
     }
 }
 $userID = $_REQUEST["userID"];
+$jobID = $_REQUEST["jobID"];
 $yearArray = array();
 for($x=0; $x<count($languages); $x++){
     array_push($yearArray, $_REQUEST[$required_params[$x+1]]);
 }
 //emoIntel, patience, adapt, projManage, probSolv, teamwork, interpersonal, leadership, time, decisiveness
 $softSkillArray = array();
-for($x=count($languages)+1; $x<count($required_params); $x++){
+//Goes up to count-2 to get rid of 'isCompany' and 'jobID'
+for($x=count($languages)+1; $x<count($required_params)-2; $x++){
     if($_REQUEST[$required_params[$x]] == 'true'){
         array_push($softSkillArray, $required_params[$x]);
     }
 }
+$isCompany = $_REQUEST["isCompany"];
 
-//When the user submits the skills page, the skills already in UserSkills for that user need to be deleted.
-try{
-    $result = skills::deleteSkillsForUser($userID);
-    if(!$result){
-        ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later.");
-    }else{
-        $result = skills::insertSkillsForUser($yearArray, $userID);
+if($isCompany == "Yes"){
+    try{
+        $result = skills::deleteSkillsForJob($jobID);
         if(!$result){
-            ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later");
+            ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later.");
         }else{
-            if(!count($softSkillArray) == 0){
-                $result = skills::insertSoftSkills($softSkillArray, $userID);
-                if(!$result){
-                    ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later.");
-                }else{
-                    ApiResponseGenerator::generate_response_json(201, ["message" => "Successfully inserted skills."]);
-                }   
-            }            
+            $result = skills::insertSkillsForJob($yearArray, $jobID);
+            if(!$result){
+                ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later.");
+            }else{
+                if(!count($softSkillArray) == 0){
+                    $result = skills::insertSoftSkillsJob($softSkillArray, $jobID);
+                    if(!$result){
+                        ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later.");
+                    }else{
+                        ApiResponseGenerator::generate_response_json(201, ["message" => "Successfully inserted skills"]);
+                    }
+                }
+            }
         }
+    }catch(Exception $exception){
+        ApiResponseGenerator::generate_error_json(500, "There was an error with the database. {$exception->getMessage()} Please try again later.");
     }
-}catch(Exception $exception){
-    ApiResponseGenerator::generate_error_json(500, "There was an error with the databse. {$exception->getMessage()} Please try again later.");
+}else{
+    //When the user submits the skills page, the skills already in UserSkills for that user need to be deleted.
+    try{
+        $result = skills::deleteSkillsForUser($userID);
+        if(!$result){
+            ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later.");
+        }else{
+            $result = skills::insertSkillsForUser($yearArray, $userID);
+            if(!$result){
+                ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later");
+            }else{
+                if(!count($softSkillArray) == 0){
+                    $result = skills::insertSoftSkills($softSkillArray, $userID);
+                    if(!$result){
+                        ApiResponseGenerator::generate_error_json(500, "There was an error with the database. Please try again later.");
+                    }else{
+                        ApiResponseGenerator::generate_response_json(201, ["message" => "Successfully inserted skills."]);
+                    }   
+                }            
+            }
+        }
+    }catch(Exception $exception){
+        ApiResponseGenerator::generate_error_json(500, "There was an error with the databse. {$exception->getMessage()} Please try again later.");
+    }
 }
