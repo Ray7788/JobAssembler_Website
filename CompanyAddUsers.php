3f074474ab1f086bd1e417f82e289ba8605e0629
Ray7788 <ray778@foxmail.com>
2022-03-30 10:05:49 +0100

    Styling finished

--- CompanyAddUsers.php
+++ CompanyAddUsers.php
@@ -60,7 +60,7 @@ $noOfUsers = count($usersApplied);
     <head>
         <title>Add Users To Company - JobAssembler</title>
         <meta name="viewport" content="width=device-width, intitial-scale=1">
-        <!--Bootstrap CSS-->
+        <!--Bootstrap CSS 4.6.1-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
         
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
@@ -91,57 +91,179 @@ $noOfUsers = count($usersApplied);
         </script>
 
         <style>
+            * {
+            padding: 0;
+            margin: 0;
+            }
+
             body {
-                    background-color: lightblue;
-                    }
-            .phpDisplay {
+                font-family: sans-serif;
+                background: url(Images/pexels-CompanyAddUsers.jpg) no-repeat
+                center fixed;
+                background-size: cover;
+            }
+            
+            .container {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin-left: 23%; 
+            }
+
+            /* .item1{
+            grid-area: header;
+            align-content: center;
+                text-align: center;
+            }
+
+            .item2{
+                grid-area: item2;
+            }
+            .item3{
+                grid-area: item3; 
+                align-content: center;
+                text-align: center;
+                vertical-align: center; 
+            }
+            .item4{
+                grid-area: item4; 
                 align-content: center;
                 text-align: center;
                 vertical-align: center;
+            } */
+
+            input {
+            font-size: 2rem;
+            padding: 1rem 2rem;
+            /* width: 600px; */
+            outline: none;
+            border: none;
+            background: rgba(255, 255, 255, 0.6);
+            border-radius: 1rem;
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
+            backdrop-filter: blur(20px) saturate(180%);
+            -webkit-backdrop-filter: blur(20px) saturate(180%);
+            z-index: 1;
             }
+
+            #input1 {
+                width: 600px;  
+            }
+
+            #input2 {
+                width: 200px;  
+            }
+
+            .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            transition: 0.3s ease-out;
+            pointer-events: none;
+            }
+
+            input:focus + .overlay {
+            background: rgba(0, 0, 0, 0.3);
+            backdrop-filter: blur(10px);
+            -webkit-backdrop-filter: blur(10px);
+            pointer-events: auto;
+            }
+
+
+            <h1>text-align: center; margin-top:10%</h1>
+
+            
+            /* For Logo border*/
+            .d-inline-block-align-top {border-radius: 5px;}
+
         </style>
 
     </head>
     <body>
-        <h1 style="text-align: center; margin-top:10%">Add Users To Company</h1>
-        <form action="CompanyAddUsers.php" method="GET" name="searchForm" style="align-items: center;">
-            <table style="margin-left: 40%; margin-top: 20px">
-                <tr>
-                    <td><input type="text" name="txt" value="<?php echo isset($_GET['txt']) ? $_GET['txt'] : ''; ?>" placeholder="Enter account Username, Forename or Surname" /></td>
-                    <td><input type="submit" name="" value="Search" /></td>
-                </tr>
-            </table>
-        </form>   
-        <b><i><p id="errorMsg"></p></i></b>
+        <!-- Nav bar -->
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+            <!-- Brand LOGO -->
+            <a class="navbar-brand">
+                <img src="Images/Logo1.png" width="30" height="30" class="d-inline-block-align-top" alt="Logo";>
+            </a>
+            
+            <ul class="navbar-nav" style="margin-left: 65%;">
+            <!-- Links -->
+                <li class="nav-item">
+                    <a class="nav-link" href="ApplicantList.php">Applicant List</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="JobCreation.php">Job Creation</a>
+                </li>
+            </ul>
+        </nav>
 
-        <p style="text-align: center;">"Please search for the account you're trying to add by their Username, Forename or Surname."</p>
-        <div class="phpDisplay">
-        <?php
-            //Show the user the keywords they have entered
-            //If the txt variable is empty then everything is displayed.
-            if($noOfUsers > 0){
-                echo("The search returned: <b>" . $noOfUsers . " </b>results. <br>");
-                if(!empty($txt)){
-                    echo("You searched for: <b>'" . $wordsForDisplay . "'</b>");
-                }
-                echo("<br><hr>");                
-                for($x=0;$x<$noOfUsers;$x++){
-                    echo('
-                    <h3>'.$usersApplied[$x][1].'</h3>
-                    '.$usersApplied[$x][2].' '.$usersApplied[$x][3].'<br>
-                    <button class="btn btn-outline-primary id="accept'.$usersApplied[$x][0].'" onclick="acceptPressed('.$usersApplied[$x][0].')">Accept User Into Company</button>
-                    <hr>');
-                }
-            }else{
-                if(empty($txt)){
-                    echo("<br>There are no users who have applied to your company that you haven't already dealt with.");
+        <!-- Main part -->
+        <div class="container">
+        
+
+        <div class="item1">
+            <header>
+            <h1>Add Users To Company</h1>
+            <hr>
+            <p>"Please search for the account you're trying to add by their Username, Forename or Surname."</p>
+             </header>
+        </div>
+            
+        <div class="item2">
+            <form action="CompanyAddUsers.php" method="GET" name="searchForm" style="align-items: center;">
+                <table style=" margin-left: -5%; margin-top: 20px">
+                    <tr>
+                        <td><input type="text" id="input1" name="txt" value="<?php echo isset($_GET['txt']) ? $_GET['txt'] : ''; ?>" placeholder="Enter account Username, Forename or Surname" /></td>
+                        <td><input type="submit" id="input2" name="" value="Search" /></td>
+                    </tr>
+                </table>
+            </form> 
+        </div>
+       
+        <div class="item3">
+            <?php
+                //Show the user the keywords they have entered
+                //If the txt variable is empty then everything is displayed.
+                if($noOfUsers > 0){
+                    echo("The search returned: <b>" . $noOfUsers . " </b>results. <br>");
+                    if(!empty($txt)){
+                        echo("You searched for: <b>'" . $wordsForDisplay . "'</b>");
+                    }
+                    echo("<br><hr>");                
+                    for($x=0;$x<$noOfUsers;$x++){
+                        echo('
+                        <h3>'.$usersApplied[$x][1].'</h3>
+                        '.$usersApplied[$x][2].' '.$usersApplied[$x][3].'<br>
+                        <button class="btn btn-outline-primary id="accept'.$usersApplied[$x][0].'" onclick="acceptPressed('.$usersApplied[$x][0].')">Accept User Into Company</button>
+                        <hr>');
+                    }
                 }else{
-                    echo("<br>There were no results for your search. Please ensure you've entered the correct information<br>");
-                    echo("You searched for: <b>'" . $wordsForDisplay . "'</b>");
+                    if(empty($txt)){
+                        echo("<br>There are no users who have applied to your company that you haven't already dealt with.");
+                    }else{
+                        echo("<br>There were no results for your search. Please ensure you've entered the correct information<br>");
+                        echo("You searched for: <b>'" . $wordsForDisplay . "'</b>");
+                    }
                 }
-            }
-        ?>
+            ?>
+
+        <div class="item4"> 
+                    <b><i><p id="errorMsg"></p></i></b>
         </div>
 
+        <div class="overlay"></div>
+
+
+    
+    </div>
     </body>
 </html>
