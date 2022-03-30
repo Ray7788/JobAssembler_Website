431b0f8f1a76d407c1aaf421942345e3defaecd0
Ray7788 <ray778@foxmail.com>
2022-03-30 21:49:18 +0100

    Fix the navbar's position

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+$database_host = "dbhost.cs.man.ac.uk";
+$database_user = "p00331rx";
+$database_pass = "X17Group";
+$database_name = "p00331rx";
+$group_dbnames = array(
+    "2021_comp10120_x17",
+);
+?>
