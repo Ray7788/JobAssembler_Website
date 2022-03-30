4615715cb37ecf1fba4551f55adf3ce1cb177694
Kv1n20 <kavin.gunasekara@student.manchester.ac.uk>
2022-03-30 16:22:10 +0100

    index pic centered

--- index.php
+++ index.php
@@ -174,7 +174,7 @@
 
     <!-- page3 -->
     <div class="container-fluid">
-            <div class="page3" style="padding:5%">
+            <div class="page3" style="vertical-align:center">
 
             <h1 class="display-4" style="color: #000000;"><b>Our Newest Jobs:</b></h1>
 
